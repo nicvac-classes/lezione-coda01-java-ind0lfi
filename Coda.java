9885cef4e57242d8c6740cec8f421ce284628d39
DavDem <davidedemarco088@gmail.com>
2026-05-04 14:15:26 +0000

    indolfi1

--- Coda.java
+++ Coda.java
@@ -1,1 +1,9 @@
 //Implementare qui la classe Coda
+public class Coda<T> {
+    private Nodo<T> head;
+    private Nodo<T> tail;
+
+    public Coda() {
+        this.head = null;
+        this.tail = null;
+    }
