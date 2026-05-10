ff392907105d36029ec6ee98c8535ec414a248d4
DavDem <davidedemarco088@gmail.com>
2026-05-10 12:26:54 +0000

    inodlfi2

--- Coda.java
+++ Coda.java
@@ -7,3 +7,21 @@ public class Coda<T> {
         this.head = null;
         this.tail = null;
     }
+    public boolean isEmpty(){
+        if(head.dato==null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public int size(){
+        int c=0;
+        Nodo<T> curr=head;
+        while(curr!=null){
+            curr=curr.next;
+            c++;
+
+        }
+        return c;
+    }
+}
